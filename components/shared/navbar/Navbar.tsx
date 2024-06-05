45cea9c28eb2aee630afb7547043c5f17d58d0b7
aljapri <gamer2mohamad@gmail.com>
2024-06-05 21:04:53 +0300

    last edite

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -48,11 +48,13 @@ const Navbar = () => {
         scrolled && "bg-white shadow-2xl text-gray-900"
       }`}
     >
-      <div className={`gpt3__navbar-links w-full `}>
+      <div
+        className={`gpt3__navbar-links w-full  flex flex-row justify-center `}
+      >
         <div className="gpt3__navbar-links_logo">
           <h1 className="text-3xl">{i("title")}</h1>
         </div>
-        <div className="gpt3__navbar-links_container">
+        <div className="gpt3__navbar-links_container ">
           {headerMenu.map((el) => {
             return (
               <MenuXl
@@ -63,9 +65,15 @@ const Navbar = () => {
               />
             );
           })}
+          <div className=" flex flex-row space-x-5 justify-center  ">
+            <p className="cursor-pointer">
+              <Link href={`/ar`}>العربية</Link>
+            </p>
+            <Link href={`/en`}>english</Link>
+          </div>
         </div>
-        
       </div>
+
       {/* // return <MenuXl name={el.name} link={el.link} key={el.link} />; */}
       <div className="gpt3__navbar-menu">
         {toggleMenu ? (
@@ -87,6 +95,12 @@ const Navbar = () => {
               {headerMenu.map((el) => {
                 return <MenuSm name={el.name} link={el.link} key={el.link} />;
               })}
+            </div>
+            <div className=" flex flex-col space-x-5 justify-center  text-gray-900 ">
+              <p className="cursor-pointer">
+                <Link href={`/ar`} className="text-gray-900">العربية</Link>
+              </p>
+              <Link href={`/en`}>english</Link>
             </div>
           </div>
         )}
